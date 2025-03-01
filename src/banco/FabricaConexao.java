b96ae5fed1139634073ccf8e632393635fe86919
Natan Paulo <natanpaulo1300@gmail.com>
2025-03-01 19:15:33 -0300

    Edição de tela principal

--- src/banco/FabricaConexao.java
+++ src/banco/FabricaConexao.java
@@ -19,7 +19,7 @@ public class FabricaConexao {
 	
 	return conexao;
 	
-
+	
 	}
 	
 }
--- src/dominio/Cosmetico.java
+++ src/dominio/Cosmetico.java
@@ -1,5 +1,44 @@
 package dominio;
 
 public class Cosmetico {
-
+	private int id;
+	private double valor;
+	private String nome;
+	private String tipo;
+	private int idmarca;
+	
+	public Cosmetico() {
+		this.idmarca = 1;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public int getIdmarca() {
+		return idmarca;
+	}
+	public void setIdmarca(int idmarca) {
+		this.idmarca = idmarca;
+	}
 }
